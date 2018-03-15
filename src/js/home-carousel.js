c6c1342cf35327b44dc72770ee941a8824199fc2
Raúl Marty <santor3medio@gmail.com>
2018-03-15 15:53:45 -0600

    #14. Add counter function and properties to include the cpunter of items in homepage carousel to file home-carousel.js

--- src/js/home-carousel.js
+++ src/js/home-carousel.js
@@ -6,6 +6,9 @@ treatmentsCarousel.owlCarousel ({
   nav: true,
   navText: ['<img class="navigation-arrows" src="img/icons/icon-left-rhombus.png" />', '<img class="navigation-arrows" src="img/icons/icon-right-rhombus.png" />'],
   dots: true,
+  margin: 20,
+  onInitialized: counter,
+  onTranslated: counter,
   responsive:{
     0:{
       items:1,
@@ -22,3 +25,11 @@ treatmentsCarousel.owlCarousel ({
 treatmentsCarousel.each(function(index) {
   $(this).find('.owl-nav, .owl-dots').wrapAll("<div class='home-controls'></div>");
 });
+
+function counter (event) {
+  var element = event.target;         // DOM element, in this example .owl-carousel
+  var items = event.item.count;     // Number of items
+  var item = event.item.index + 1;     // Position of the current item
+  $('#home-counter-init').html("0" + item)
+  $('#home-counter-total').html("0" + items)
+}
